c672609ce1976690f4cab9b8c1b97ba70bbcb38b
maxbhs <max_barcatmarti@msn.com>
2017-03-21 22:40:46 +0100

    second commit

--- Prince/Player.cpp
+++ Prince/Player.cpp
@@ -20,32 +20,35 @@ enum PlayerAnims
 void Player::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 {
 	bJumping = false;
-	spritesheet.loadFromFile("images/bub.png", TEXTURE_PIXEL_FORMAT_RGBA);
-	sprite = Sprite::createSprite(glm::ivec2(32, 32), glm::vec2(0.25, 0.25), &spritesheet, &shaderProgram);
+	spritesheet.loadFromFile("sprites/prince-sprite.png", TEXTURE_PIXEL_FORMAT_RGBA);
+	sprite = Sprite::createSprite(glm::ivec2(60,55), glm::vec2(0.0909f, 0.5f), &spritesheet, &shaderProgram);
 	sprite->setNumberAnimations(5);
 	
 		sprite->setAnimationSpeed(STAND_LEFT, 8);
 		sprite->addKeyframe(STAND_LEFT, glm::vec2(0.f, 0.f));
 		
 		sprite->setAnimationSpeed(STAND_RIGHT, 8);
-		sprite->addKeyframe(STAND_RIGHT, glm::vec2(0.25f, 0.0f));
+		sprite->addKeyframe(STAND_RIGHT, glm::vec2(0.0f, 0.0f));
 		
 		sprite->setAnimationSpeed(MOVE_LEFT, 8);
 		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.f));
-		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.25f));
-		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.5f));
-		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.75f));
+		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.f));
+		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.f));
+		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.f));
 		
 		sprite->setAnimationSpeed(MOVE_RIGHT, 8);
-		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25f, 0.f));
-		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25f, 0.25f));
-		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25f, 0.5f));
-		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.5f, 0.75f));
+		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.0f, 0.6f));
+		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.1f, 0.6f));
+		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.2f, 0.6f));
+		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.3f, 0.6f));
+		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.4f, 0.6f));
+		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.5f, 0.6f));
+		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.6f, 0.6f));
 
 		sprite->setAnimationSpeed(JUMP_FALL, 8);
-		sprite->addKeyframe(JUMP_FALL, glm::vec2(0.75f, 0.f));
-		sprite->addKeyframe(JUMP_FALL, glm::vec2(0.75f, 0.25f));
-		sprite->addKeyframe(JUMP_FALL, glm::vec2(0.75f, 0.5f));
+		sprite->addKeyframe(JUMP_FALL, glm::vec2(0.f, 0.f));
+		sprite->addKeyframe(JUMP_FALL, glm::vec2(0.f, 0.f));
+		sprite->addKeyframe(JUMP_FALL, glm::vec2(0.f, 0.f));
 
 		
 	sprite->changeAnimation(0);
